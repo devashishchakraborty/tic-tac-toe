6348fc916b5aeef0471814cbbc7bf9108b4d22bd
Devashish Chakraborty <devashishchakra@gmail.com>
2023-03-15 01:13:53 +0530

    Added a gameActive flag which stops the game if anyone wins

--- script.js
+++ script.js
@@ -1,86 +1,91 @@
 const gameBoard = (() => {
-    let board = [["","",""], ["","",""], ["","",""]];
+    let board = [["", "", ""], ["", "", ""], ["", "", ""]];
 
-    const setBoard = function(row, column, turn){
+    const setBoard = function (row, column, turn) {
         board[row][column] = turn;
     }
 
     const getBoard = () => board;
 
-    const clearBoard = function(){
-        board = [["","",""], ["","",""], ["","",""]];
+    const clearBoard = function () {
+        board = [["", "", ""], ["", "", ""], ["", "", ""]];
     }
-    return { getBoard, setBoard, clearBoard}
+    return { getBoard, setBoard, clearBoard }
 })();
 
 const displayController = (() => {
     const boardCells = document.querySelectorAll(".boardCell");
     const restartButton = document.querySelector(".restart");
     let turn = "X";
+    let gameActive = true;
 
     const restartBoard = () => {
-        restartButton.addEventListener("click", function(){
+        restartButton.addEventListener("click", function () {
             gameBoard.clearBoard()
-            boardCells.forEach(function(boardCell){
+            boardCells.forEach(function (boardCell) {
                 boardCell.textContent = "";
             });
-    });
+            gameActive = true;
+        });
     }
 
     const updateBoard = () => {
         boardCells.forEach(function (boardCell) {
             boardCell.addEventListener("click", function (event) {
-                if (boardCell.textContent === ""){
+                if (gameActive) {
+                    if (boardCell.textContent === "") {
 
-                    const elementRow = boardCell.getAttribute("row");
-                    const elementColumn = boardCell.getAttribute("column");
-                    
-                    gameBoard.setBoard(elementRow, elementColumn, turn);
-                    boardCell.textContent = gameBoard.getBoard()[elementRow][elementColumn];
+                        const elementRow = boardCell.getAttribute("row");
+                        const elementColumn = boardCell.getAttribute("column");
 
-                    const winner = checkResult(gameBoard.getBoard(), turn);
-                    if (winner) {
-                        console.log(`Player ${winner} wins`);
+                        gameBoard.setBoard(elementRow, elementColumn, turn);
+                        boardCell.textContent = gameBoard.getBoard()[elementRow][elementColumn];
+
+                        winner = checkResult(gameBoard.getBoard(), turn);
+                        if (winner) {
+                            console.log(`Player ${winner} wins`);
+                            gameActive = false;
+                        }
+
+                        if (turn === "X") turn = "O";
+                        else if (turn === "O") turn = "X";
                     }
-
-                    if (turn === "X") turn = "O";
-                    else if (turn === "O") turn = "X";
                 }
             })
         });
+        restartBoard();
     }
 
-    return {updateBoard, restartBoard};
+    return { updateBoard };
 })();
 
-const checkResult = function(board, player){
-    if (board[0][0] === player && board[0][1] === player && board[0][2] === player){
+const checkResult = function (board, player) {
+    if (board[0][0] === player && board[0][1] === player && board[0][2] === player) {
         return player;
     }
-    else if (board[1][0] === player && board[1][1] === player && board[1][2] === player){
+    else if (board[1][0] === player && board[1][1] === player && board[1][2] === player) {
         return player;
     }
-    else if (board[2][0] === player && board[2][1] === player && board[2][2] === player){
-        return player;
-    }
-
-    else if (board[0][0] === player && board[1][0] === player && board[2][0] === player){
-        return player;
-    }
-    else if (board[0][1] === player && board[1][1] === player && board[2][1] === player){
-        return player;
-    }
-    else if (board[0][2] === player && board[1][2] === player && board[2][2] === player){
+    else if (board[2][0] === player && board[2][1] === player && board[2][2] === player) {
         return player;
     }
 
-    else if (board[0][0] === player && board[1][1] === player && board[2][2] === player){
+    else if (board[0][0] === player && board[1][0] === player && board[2][0] === player) {
         return player;
     }
-    else if (board[2][0] === player && board[1][1] === player && board[0][2] === player){
+    else if (board[0][1] === player && board[1][1] === player && board[2][1] === player) {
+        return player;
+    }
+    else if (board[0][2] === player && board[1][2] === player && board[2][2] === player) {
+        return player;
+    }
+
+    else if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
+        return player;
+    }
+    else if (board[2][0] === player && board[1][1] === player && board[0][2] === player) {
         return player;
     }
 }
 
 displayController.updateBoard();
-displayController.restartBoard();
