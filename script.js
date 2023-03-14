d879a7e1480e782dfd5c0d15cff2e84065e5116b
Devashish Chakraborty <devashishchakra@gmail.com>
2023-03-14 17:41:46 +0530

    Added a restart board function to clear everything on the board and play again. Also added the logic to check which side won the game in checkResult function

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 const gameBoard = (() => {
-    const board = [];
-    for (let i = 0; i < 3; i++) {
-        board[i] = [];
-        for (let j = 0; j < 3; j++) {
-            board[i].push("")
-        }
-    }
+    let board = [["","",""], ["","",""], ["","",""]];
+
     const setBoard = function(row, column, turn){
         board[row][column] = turn;
     }
 
     const getBoard = () => board;
-    return { getBoard, setBoard }
+
+    const clearBoard = function(){
+        board = [["","",""], ["","",""], ["","",""]];
+    }
+    return { getBoard, setBoard, clearBoard}
 })();
 
 const displayController = (() => {
     const boardCells = document.querySelectorAll(".boardCell");
+    const restartButton = document.querySelector(".restart");
     let turn = "X";
+
+    const restartBoard = () => {
+        restartButton.addEventListener("click", function(){
+            gameBoard.clearBoard()
+            boardCells.forEach(function(boardCell){
+                boardCell.textContent = "";
+            });
+    });
+    }
+
     const updateBoard = () => {
         boardCells.forEach(function (boardCell) {
             boardCell.addEventListener("click", function (event) {
@@ -27,7 +37,12 @@ const displayController = (() => {
                     
                     gameBoard.setBoard(elementRow, elementColumn, turn);
                     boardCell.textContent = gameBoard.getBoard()[elementRow][elementColumn];
-    
+
+                    const winner = checkResult(gameBoard.getBoard(), turn);
+                    if (winner) {
+                        console.log(`Player ${winner} wins`);
+                    }
+
                     if (turn === "X") turn = "O";
                     else if (turn === "O") turn = "X";
                 }
@@ -35,7 +50,37 @@ const displayController = (() => {
         });
     }
 
-    return {updateBoard};
+    return {updateBoard, restartBoard};
 })();
 
+const checkResult = function(board, player){
+    if (board[0][0] === player && board[0][1] === player && board[0][2] === player){
+        return player;
+    }
+    else if (board[1][0] === player && board[1][1] === player && board[1][2] === player){
+        return player;
+    }
+    else if (board[2][0] === player && board[2][1] === player && board[2][2] === player){
+        return player;
+    }
+
+    else if (board[0][0] === player && board[1][0] === player && board[2][0] === player){
+        return player;
+    }
+    else if (board[0][1] === player && board[1][1] === player && board[2][1] === player){
+        return player;
+    }
+    else if (board[0][2] === player && board[1][2] === player && board[2][2] === player){
+        return player;
+    }
+
+    else if (board[0][0] === player && board[1][1] === player && board[2][2] === player){
+        return player;
+    }
+    else if (board[2][0] === player && board[1][1] === player && board[0][2] === player){
+        return player;
+    }
+}
+
 displayController.updateBoard();
+displayController.restartBoard();
